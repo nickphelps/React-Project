0854700063b0c88eb4154c76d237685067543f95
Nick Phelps <Nick90052@Gmail.com>
2019-03-13 21:26:15 -0500

    react-sidebar

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Jumbotron, ListGroup, Button, Nav } from 'react-bootstrap'
+import { Container, Row, Col, Jumbotron, ListGroup, Button } from 'react-bootstrap'
 import DashboardHomeTab from '../components/DashboardHomeTab'
 import LoginFormContainer from './LoginFormContainer';
 import { connect } from 'react-redux'
 import DogTab from '../components/DogTab'
 import UIContainer from './UIContainer'
 import NavBar from '../containers/NavBarContainer';
+import { SideNav, Nav } from 'react-sidenav'
+import styled from 'styled-components'
+import { 
+        AppContainer as BaseAppContainer,
+        ExampleNavigation as BaseNavigation,
+        Body as Body
+
+ } from '../components/StyleContainers'
+ import { Icon } from 'react-icons-kit'
+ import { dashboard } from 'react-icons-kit/fa/dashboard'
+ import { user } from 'react-icons-kit/fa/user'
+
+
+const AppContainer = styled(BaseAppContainer)`
+    height: 100vh;`
+
+const Navigation = styled(BaseNavigation)`
+    background: #303641;
+    color: #8d97ad;
+    font-size: 1em;
+    letter-spacing: 2px;
+    width: 240px;
+    line-height: 22px;
+  `;
+
+const IconCnt = styled.div`
+  color: #6a56a5;
+  display: flex;
+  justify-content: center;
+  aligh-items: center;
+`;
+
+const theme = {
+    selectionColor: "#FFF",
+    hoverBgColor: "#181b20"
+  }
+
+const Text = styled.div`
+  padding-left: 8px;
+`;
 
 class DashboardContainer extends Component {
     constructor(props) {
@@ -18,13 +58,17 @@ class DashboardContainer extends Component {
 
     handleDashboardHomeTabClick (event) {
         event.preventDefault()
+        console.log('enterd home')
         this.setState({
             tabState: 1
         })
+        
     }
 
     handleLoginFormContainerClick (event) {
         event.preventDefault()
+        console.log('enterd Login')
+
         this.setState({
             tabState: 2
         })
@@ -53,42 +97,58 @@ class DashboardContainer extends Component {
       console.log(this.props.userInfo, 'my Redux Store')
       console.log(this.state)
     return (
-        <div>
-        <div> <NavBar /> </div>
-        <Container variant='flush'>
-        <Row>
-        <Col xs={3} >
-        <ListGroup defaultActiveKey="#link1">
-            <ListGroup.Item action href={<DashboardHomeTab />}
-                            onClick={(event) => this.handleDashboardHomeTabClick(event)} >
-            Home
-            </ListGroup.Item>
-            <ListGroup.Item action href={<LoginFormContainer />}
-                            onClick={(event) => this.handleLoginFormContainerClick(event)} >
-            Change User Settings
-            </ListGroup.Item>
-            <ListGroup.Item action href={<DogTab />}
-                            onClick={(event) => this.handleDogTabClick(event)}>
-            Dog Pics!
-            </ListGroup.Item>
-            <ListGroup.Item action href={<DogTab />}
-                            onClick={(event) => this.handleUITabClick(event)}>
-            Change UI
-            </ListGroup.Item>
-            
-    </ListGroup>
-        </Col>
-        <Col>
-        <Jumbotron style={{backgroundColor: '#ffffff'}}>
-            {this.state.tabState === 1 ? <DashboardHomeTab store={this.props.userInfo} /> : null}
-            {this.state.tabState === 2 ? <LoginFormContainer /> : null}
-            {this.state.tabState === 3 ? <DogTab /> : null}
-            {this.state.tabState === 4 ? <UIContainer /> : null}
-            <Button variant='primary' onClick={(event) => this.HandleGoHomeClick(event)}>Go Home</Button>
-        </Jumbotron>
-        </Col>
-        </Row>
-    </Container>
+    <div>
+    <NavBar /> 
+        <AppContainer>
+            <Navigation>
+                <SideNav defaultSelectedPath={<DashboardHomeTab />}
+                        theme={theme}
+                >
+                    <div onClick={(event) => this.handleDashboardHomeTabClick(event)}>
+                        <Nav>
+                            <IconCnt>
+                                <Icon icon={dashboard} />
+                            </IconCnt>
+                            <Text>Home Tab</Text>
+                        </Nav>
+                    </div>
+                    <div onClick={(event) => this.handleLoginFormContainerClick(event)}> 
+                        <Nav>
+                            <IconCnt>
+                                <Icon icon={user} />
+                            </IconCnt>
+                            <Text>Change Users</Text>
+                        </Nav>
+                    </div>
+                    <div onClick={(event) => this.handleDogTabClick(event)}>
+                        <Nav>
+                            <IconCnt>
+                                    <Icon icon={user} />
+                            </IconCnt>
+                            <Text>Dog Pics!</Text>
+                        </Nav>
+                    </div>
+                    <div onClick={(event) => this.handleUITabClick(event)}>
+                        <Nav>
+                            <IconCnt>
+                                    <Icon icon={user} />
+                            </IconCnt>
+                            <Text>Change UI</Text>
+                        </Nav>
+                    </div>
+
+                </SideNav>
+            </Navigation>
+            <Body>
+            <Jumbotron style={{backgroundColor: '#ffffff'}}>
+                {this.state.tabState === 1 ? <DashboardHomeTab store={this.props.userInfo} /> : null}
+                {this.state.tabState === 2 ? <LoginFormContainer /> : null}
+                {this.state.tabState === 3 ? <DogTab /> : null}
+                {this.state.tabState === 4 ? <UIContainer /> : null}
+                <Button variant='primary' onClick={(event) => this.HandleGoHomeClick(event)}>Go Home</Button>
+            </Jumbotron>
+            </Body>
+        </AppContainer>
     </div>
     )
   }
